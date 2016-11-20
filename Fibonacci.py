6050f699a50ef3eb2354b207711a908cb6d07466
PC\Administrator <bibibobo2511@gmail.com>
2016-11-20 16:37:28 +0700

    Fix bien c=i

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,9 @@
 a=0
 b=1
-c=0
+i=0
 for i in range(5):
     z=a+b
-    print("Month{0}:{1} pair(s) of rabbits".format(c,z))
-    c+=1
+    print("Month{0}:{1} pair(s) of rabbits".format(i,z))
     a=b
     b=z
 #day so fibonacci
