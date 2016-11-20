adaf4577eb0e309f2938e8ebd0fdf4e2191157e3
PC\Administrator <bibibobo2511@gmail.com>
2016-11-20 16:33:37 +0700

    Fix  HW1

--- Bacterias.py
+++ Bacterias.py
@@ -2,6 +2,7 @@ x=input("How many B bacterias are there?")
 y=input("How much time will we wait")
 a=int(x)
 b=int(y)
-z=a*(2**(b/2))
+d=b//2
+z=a*(2**d)
 c=int(z)
 print("After 2 minutes we would have {0} bacterias".format(c))
